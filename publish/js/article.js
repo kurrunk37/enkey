cf46753ff0baf5779f12941188f7c4444b7e79a5
huzhengquan <huzhengquan@gmail.com>
2018-04-04 13:34:13 +0800

    fix

--- publish/js/article.js
+++ publish/js/article.js
@@ -7,7 +7,7 @@ function check_begin(){
 	var waitDom=$('#art span.wait:first');
 	waitDom.addClass('next');
 	if(waitDom.length==0){
-		$('#time_sum').text(($('#art span').length/Math.round((new Date()-window.time_begin)/1000/60)).toFixed(2)+'字 / 分钟');
+		$('#time_sum').text(($('#art span').length/(new Date()-window.time_begin)*1000*60).toFixed(2)+'字 / 分钟');
 		$('#restart_bt>div').slideDown();
 		var pos_top=$('#buttons').position().top- parseInt($(window).height()/2);
 		if(pos_top>0) $("html,body").animate({scrollTop:pos_top}, 1000);
